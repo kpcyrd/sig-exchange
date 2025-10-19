5a44485702d9fa2338c9dc86ac60f67e8ec2d961
kpcyrd <git@rxv.cc>
2025-10-19 21:20:10 +0200

    Allow download of pgp keys

--- src/issuer.rs
+++ src/issuer.rs
@@ -1,8 +1,32 @@
+use crate::errors::*;
+use sequoia_openpgp::{
+    armor,
+    serialize::stream::{Armorer, Message},
+};
 use serde::Serialize;
+use std::io::Write;
 
 #[derive(sqlx::FromRow, Debug, Serialize, PartialEq)]
 pub struct Issuer {
     pub fingerprint: String,
     pub family: String,
     pub key: Option<Vec<u8>>,
+}
+
+impl Issuer {
+    // XXX: this only makes sense for PGP keys
+    pub fn to_ascii_armored(&self) -> Result<String> {
+        let mut sink = Vec::new();
+
+        let bytes = self.key.as_ref().context("Issuer has no key data")?;
+        {
+            let message = Message::new(&mut sink);
+            let mut message = Armorer::new(message).kind(armor::Kind::PublicKey).build()?;
+            message.write_all(bytes)?;
+            message.finalize()?;
+        }
+
+        let armored = String::from_utf8(sink)?;
+        Ok(armored)
+    }
 }
--- src/web/issuer.rs
+++ src/web/issuer.rs
@@ -6,7 +6,7 @@ use sequoia_openpgp::{Packet, PacketPile, parse::Parse};
 use serde::{Deserialize, Serialize};
 use std::result;
 use std::sync::Arc;
-use warp::http::Uri;
+use warp::http::{StatusCode, Uri};
 
 fn pgp_mpis(bytes: &[u8]) -> Option<String> {
     let pile = PacketPile::from_reader(bytes).ok()?;
@@ -28,11 +28,23 @@ pub(super) async fn get(
     db: db::Client,
     fingerprint: String,
 ) -> result::Result<Box<dyn warp::Reply>, warp::Rejection> {
-    let Some(mut issuer) = db.get_issuer(&fingerprint).await? else {
+    let (fingerprint, download) = fingerprint
+        .strip_suffix(".asc")
+        .map(|c| (c, true))
+        .unwrap_or((&fingerprint, false));
+
+    let Some(mut issuer) = db.get_issuer(fingerprint).await? else {
         return Err(warp::reject::not_found());
     };
-    let upstreams = db.list_upstreams_for_issuer(&fingerprint).await?;
-    let sigs = db.list_sigs_for_issuer(&fingerprint).await?;
+
+    if download {
+        let armored = issuer.to_ascii_armored().map_err(ApiError::from)?;
+        let response = warp::reply::with_status(armored, StatusCode::OK);
+        return Ok(Box::new(response));
+    }
+
+    let upstreams = db.list_upstreams_for_issuer(fingerprint).await?;
+    let sigs = db.list_sigs_for_issuer(fingerprint).await?;
 
     let mpis = match (issuer.family.as_str(), issuer.key.take()) {
         ("pgp", Some(key)) => pgp_mpis(&key),
